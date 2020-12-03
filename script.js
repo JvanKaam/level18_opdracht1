418ea865a904f522c6811699e03507c6c2a75943
JvanKaam <65725956+JvanKaam@users.noreply.github.com>
2020-12-03 21:15:49 +0100

    commit script

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+const fivePlusSeven = () => 5 + 7;
+
+
+
+console.log(fivePlusSeven());
+
+
+const myFunction = (a, b) => a + b;
+
+console.log(myFunction(5, 8));
+
+const addFive = a => a + 5;
+
+console.log(addFive(4));
+
+
+
+
+
+
+const createObject = () => ({ greeting: "hoi" });
+
+console.log(createObject());
